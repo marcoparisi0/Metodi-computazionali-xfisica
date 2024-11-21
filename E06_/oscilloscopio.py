be677c948ce46a0dc3b236e25e2577a274c15bb1
marcoparisi0 <marcopar22@gmail.com>
2024-11-21 20:44:31 +0100

    modifiche

--- E06_/oscilloscopio.py
+++ E06_/oscilloscopio.py
@@ -7,41 +7,48 @@ import argparse
 
 data=pd.read_csv('oscilloscope.csv')
 dati=data[['time','signal1','signal2']]
-t=dati['time']
-c1=dati['signal1']
-c2=dati['signal2']
-
-plt.plot(t,c1,color= 'blue')
-plt.xlabel('tempo')
-plt.ylabel('voltaggio')
-plt.title('segnale oscilloscopio CH1')
-plt.show()
-plt.plot(t,c2,color='green')
-plt.xlabel('tempo')
-plt.ylabel('voltaggio')
-plt.title('segnale oscilloscopio CH2')
-plt.show()
+t=np.array(dati['time'])
+c1=np.array(dati['signal1'])
+c2=np.array(dati['signal2'])
 
 
-def dv(xx, yy, nn):
-    d=[]
-    """for i in range(int(nn/2),len(t)-int(nn/2)):
-        cucu=(yy[i+(nn/2)] - yy[i-(nn/2)])/(xx[i+(nn/2)] - xx[i-(nn/2)])"""
-    for i in range(0,len(t)+1):
-        cucu=(yy[i+(nn)] - yy[i])/(xx[i+(nn/2)] - xx[i-(nn/2)])
+
+def dv(xx, yy, nh):
+    dd = yy[nh:] - yy[:-nh]
+    hh = xx[nh:] - xx[:-nh]
     
-        d.append(cucu)
-    return d
+    for ih in range(int(nh/2)):
+        dd = np.append(yy[nh-ih-1]-yy[0], dd)
+        dd = np.append(dd, yy[-1]-yy[-(nh-ih)])
+    
+        hh = np.append(xx[nh-ih-1]-xx[0], hh)
+        hh = np.append(hh, xx[-1]-xx[-(nh-ih)])
+    return dd/hh
 
 nh=int(input('inserire la precisione con cui calcolare la derivata (intervallo da prendere per la dfferenza centrale per aggirare il rumore):'))
 
-plt.plot(t,dv(t,c1,nh),color= 'red')
-plt.xlabel('tempo')
-plt.ylabel('voltaggio')
-plt.title('derivata segnale oscilloscopio CH1')
-plt.show()
-plt.plot(t,dv(t,c2,nh),color='purple')
-plt.xlabel('tempo')
-plt.ylabel('voltaggio')
-plt.title('derivata segnale oscilloscopio CH2')
+
+
+
+fig, axs = plt.subplots(2,2,figsize=(12,6))
+axs[0,0].plot(t,c1,color= 'blue')
+axs[0,0].set_xlabel('tempo')
+axs[0,0].set_ylabel('voltaggio')
+axs[0,0].set_title('segnale oscilloscopio CH1')
+
+axs[0,1].plot(t,c2,color='green')
+axs[0,1].set_xlabel('tempo')
+axs[0,1].set_ylabel('voltaggio')
+axs[0,1].set_title('segnale oscilloscopio CH2')
+
+axs[1,0].plot(t,dv(t,c1,nh),color= 'red')
+axs[1,0].set_xlabel('tempo')
+axs[1,0].set_ylabel('voltaggio')
+axs[1,0].set_title('derivata segnale oscilloscopio CH1')
+
+axs[1,1].plot(t,dv(t,c2,nh),color='purple')
+axs[1,1].set_xlabel('tempo')
+axs[1,1].set_ylabel('voltaggio')
+axs[1,1].set_title('derivata segnale oscilloscopio CH2')
+
 plt.show()   
