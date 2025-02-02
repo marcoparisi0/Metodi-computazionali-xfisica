68d50449752ff0d839f987eb3b8fc6ca40e60ca4
marcoparisi0 <marcopar22@gmail.com>
2025-02-02 22:28:47 +0100

    dd

--- Progetto/tramonto.py
+++ Progetto/tramonto.py
@@ -12,9 +12,10 @@ import math
 Definizione valori e funzioni utili
 
 
-!!!!probabilmente conviene cambiare le unità d misura!!!!
-
+il problema che avevo nell'esponente, è che, usando np.exp(arg)-1  ppure np.expm1(arg), veniva calcolato e^... , poi sottratto 1 e poi fatta la divisione. Questo causava overflow nel calcolo dell'esponente, essendo un numero moolto grosso.Dunque, essendo l'argomento dell'esponente >>1, ho considerato direttamente np.exp(-arg) che NON IMPLICA il calcolo di un exp con esponente gigante.  Spero vada bene
 """
+
+
 T_sun=5.75*(10**3)   #K
 T_sau=1.9*(10**3)
 T_vega=10*(10**3)
@@ -27,18 +28,23 @@ N_T=2.504*(10**25)
 S_z=8000 #m
 S_o=np.sqrt((R_T+S_z)**2 -  R_T**2)
 
-
+#spessore di massa d'aria con angolo generico
 def S_teta(t):
     return np.sqrt((R_T*np.cos(t))**2 +  2*R_T*S_z + S_z**2) - R_T*np.cos(t)
 
-
+#densità di energia irradiata
 def B(l,T):
-   return  2*h*(c**2)/(np.expm1(h*c/(l*k*T))*(l**5))
+    #return  2*h*(c**2)/(np.expm1(h*c/(l*k*T))*(l**5))   #vera
+    return 2*h*(c**2)*np.exp(-h*c/(l*k*T))/(l**5)    #approx
 
+
+# densità di fotoni per lunghezza d'onda 
 def D(l,T):
-    return 2*c/((l**4)*np.expm1(h*c/(l*k*T)))
+    #return 2*c/((l**4)*np.expm1(h*c/(l*k*T)))    #vera
+    return 2*c*np.exp(-h*c/(l*k*T))/(l**4)     #approx
 
 
+# probabilità di interazione
 def beta(l):
     return 8*pow(np.pi,3)*pow(n_T**2 - 1,2)/(3*N_T*l**4)
 
@@ -48,6 +54,7 @@ def D_scatter(l,s,T):
 
 
 la=np.random.uniform(low=10**(-8), high=5*10**(-6), size=10000)  # in  m   considero spettro da UV a infrarossi
+#la=np.random.uniform(low=1, high=5000, size=10000)
 lmbd=np.sort(la)
 
 
@@ -55,33 +62,32 @@ lmbd=np.sort(la)
 
 
 
-""""
 fig,axs = plt.subplots(2,4, figsize=(12,6))
-axs[0,0].scatter(lmbd,B(lmbd,T_sun),color='darkgoldenrod')
-axs[0,1].scatter(lmbd,B(lmbd,T_sau),color='navy')
-axs[0,2].scatter(lmbd,B(lmbd,T_vega),color='darkgreen')
-axs[0,3].scatter(lmbd,B(lmbd,T_rigel),color='firebrick')
-axs[1,0].scatter(lmbd,D(lmbd,T_sun), color='goldenrod', marker='2')
-axs[1,0].scatter(lmbd,D_scatter(lmbd,S_z,T_sun),  color='gold', marker= '2')
-axs[1,0].scatter(lmbd,D_scatter(lmbd,S_o,T_sun),  color='tan', marker= '2')
-axs[1,1].scatter(lmbd,D(lmbd,T_sau), color='slateblue', marker='2')
-axs[1,1].scatter(lmbd,D_scatter(lmbd,S_z,T_sau),  color='royalblue', marker= '2')
-axs[1,1].scatter(lmbd,D_scatter(lmbd,S_o,T_sau),  color='darkorchid', marker= '2')
-axs[1,2].scatter(lmbd,D(lmbd,T_vega), color='seagreen', marker='2')
-axs[1,2].scatter(lmbd,D_scatter(lmbd,S_z,T_vega),  color='lime', marker= '2')
-axs[1,2].scatter(lmbd,D_scatter(lmbd,S_o,T_vega),  color='forestgreen', marker= '2')
-axs[1,3].scatter(lmbd,D(lmbd,T_rigel), color='brown', marker='2')
-axs[1,3].scatter(lmbd,D_scatter(lmbd,S_z,T_rigel),  color='lightcoral', marker= '2')
-axs[1,3].scatter(lmbd,D_scatter(lmbd,S_o,T_rigel),  color='darkred', marker= '2')
-
+axs[0,0].plot(lmbd,B(lmbd,T_sun),color='darkgoldenrod')
+axs[0,1].plot(lmbd,B(lmbd,T_sau),color='navy')
+axs[0,2].plot(lmbd,B(lmbd,T_vega),color='darkgreen')
+axs[0,3].plot(lmbd,B(lmbd,T_rigel),color='firebrick')
+axs[1,0].plot(lmbd,D(lmbd,T_sun), color='goldenrod')
+axs[1,0].plot(lmbd,D_scatter(lmbd,S_z,T_sun),  color='gold')
+axs[1,0].plot(lmbd,D_scatter(lmbd,S_o,T_sun),  color='tan')
+axs[1,1].plot(lmbd,D(lmbd,T_sau), color='slateblue')
+axs[1,1].plot(lmbd,D_scatter(lmbd,S_z,T_sau),  color='royalblue')
+axs[1,1].plot(lmbd,D_scatter(lmbd,S_o,T_sau),  color='darkorchid')
+#axs[1,1].set_yscale('log')    #slateblue e royalblue sono sovrapposti, quasi
+axs[1,2].plot(lmbd,D(lmbd,T_vega), color='seagreen')
+axs[1,2].plot(lmbd,D_scatter(lmbd,S_z,T_vega),  color='lime')
+axs[1,2].plot(lmbd,D_scatter(lmbd,S_o,T_vega),  color='forestgreen')
+axs[1,3].plot(lmbd,D(lmbd,T_rigel), color='brown')
+axs[1,3].plot(lmbd,D_scatter(lmbd,S_z,T_rigel),  color='lightcoral')
+axs[1,3].plot(lmbd,D_scatter(lmbd,S_o,T_rigel),  color='darkred')
 plt.show()
+
+
 """
-
-
 #PLOT NORMALI SOLE
 plt.plot(lmbd,B(lmbd,T_sun), color='mediumvioletred')
 plt.show()
-"""
+
 plt.scatter(lmbd,D(lmbd,T_sun), color='darkgreen', marker='2')
 plt.scatter(lmbd,D_scatter(lmbd,S_z,T_sun),  color='gold', marker= '2')
 plt.scatter(lmbd,D_scatter(lmbd,S_o,T_sun),  color='purple', marker= '2')
@@ -103,10 +109,11 @@ dividere per 'colori'
 
 
 """
-per il flusso integrato integro D(l,T) tra i limiti scelti di lambda
+per il flusso integrato integro D(l,T) tra i limiti scelti di lambda, mi va in overflow !!!
 """
 
-teta=np.random.uniform(low=0,high=pi,size=1000)
+te=np.random.uniform(low=0,high=pi,size=1000)
+teta=np.sort(te)
 
 f=[]
 for t in teta:
@@ -135,18 +142,12 @@ for t in teta:
 
 
 
-
-
-
-
-"""
 fig,axs = plt.subplots(2,2, figsize=(12,6))
-axs[0,0].scatter(S_teta(teta),f, color='teal')
-axs[0,1].scatter(S_teta(teta),w, color='pink')
-axs[1,0].scatter(S_teta(teta),v, color='violet')
-axs[1,1].scatter(S_teta(teta),r, color='sienna')
+axs[0,0].plot(S_teta(teta),f, color='teal')
+axs[0,1].plot(S_teta(teta),w, color='pink')
+axs[1,0].plot(S_teta(teta),v, color='violet')
+axs[1,1].plot(S_teta(teta),r, color='sienna')
 plt.show()
-"""
 
 
 
@@ -158,11 +159,32 @@ plt.show()
 
 
 
-
+FINO A QUI OKK    (considerando buona l'approssimazione dell'esponenziale)
 DA FARE
-fare vedere distribuzione teta
+fare vedere distribuzione teta e lmbd
 fare meglio i grafici e spiegare andamenti
 """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -175,8 +197,13 @@ STELLA X
 
 PROBABILMENTE NON TORNA PERCHÈ (e in questo caso dovrei modificare anche la roba prima) sto facendo il fit con la densità di fotonii, e io qua ho il numero di fotoni, quindi...
 
-cose da fare: scrivere bene il risultato, confrontare con il grafico fittato, far vedere lo scarto
+cose da fare: scrivere bene il risultato, confrontare con il grafico fittato, far vedere lo scarto, chi quadro
+
+
 """
+
+
+
 
 dati=pd.read_csv('observed_starX.csv')
 #print(dati)
@@ -191,16 +218,25 @@ plt.plot(ll,ph, color= 'green')
 plt.show()
 
 
-ph_c=ph*np.exp(beta(l_nm)*S_teta(pi/4))  #in nm, sennò non calcola
+ph_c=ph*np.exp(beta(ll)*S_teta(pi/4))  #in nm, sennò non calcola
 
-Tx, Tx_cov = optimize.curve_fit(D,ll,ph_c,10**(-9)))
+#porco=[]
+#for i in range(len(ph)):
+ #   porco.append(ph[i]-ph_c[i])
+
+    
+#print(porco)
+
+
+
+#print(ph_c)
+Tx, Tx_cov = optimize.curve_fit(D,ll,ph_c)
 
 print(Tx)
 print(Tx_cov)
 
-
-plt.plot(ll,ph, color= 'green')
 plt.plot(ll,D(ll,Tx),color='red')
+plt.plot(ll,ph_c, color= 'blue')
 plt.show()
 
 
